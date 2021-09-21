4e114ae511318391957ad22c61c4285e2d97c0cb
Sander Wieringa <s.wieringa@student.fontys.nl>
2021-09-21 14:55:09 +0200

    new backend 11

---
+++ checkers/checkersbackend/src/test/java/checkers/CheckersApplicationTests.java
@@ -0,0 +1,13 @@
+package checkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
