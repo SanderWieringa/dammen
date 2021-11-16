7cc2b2adb242595fd42c5feaaf03c1d141b37ab3
I389783 <j.leunisse@student.fontys.nl>
2021-11-16 13:38:45 +0100

    changed colors in compliance with wishes

--- checkers/checkersfrontend/src/components/statelessBoard/BlackMan.js
+++ checkers/checkersfrontend/src/components/statelessBoard/BlackMan.js
@@ -1,11 +1,7 @@
 import React from "react";
-import blackMan from '../../black-man.svg'
+import blackMan from "../../black-man.svg";
 import "./styles.scss";
 
 export const Blackman = () => {
-    
-    return (
-        <img src={blackMan} alt={'A black man.'} className={'piece'} />
-    )
+  return <img src={blackMan} alt={"A black man."} className={"piece"} />;
 };
-  
